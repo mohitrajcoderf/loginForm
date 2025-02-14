5f389c739a091b88e803156f53509420215ec0d1
Mohit Raj <mohitraj12099@gmail.com>
2025-02-14 09:27:39 +0000

    restart

--- scripts/sitemap.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export async function getWritingIds () {
-    const files = fs.readdirSync (
-        path.join(process.cwd(), 'src/content/writings'),
-    );
-}
